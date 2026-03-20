935c77cbbe206cbfc1d85d300877445078d2e8a8
niel <74702008+danielpurwo@users.noreply.github.com>
2026-03-20 19:35:30 +0700

    dimension.py

---
+++ src/tables/dimension.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+def generate_static_dimensions():
+    # 1. Brands
+    dim_brand = pd.DataFrame([
+        {'brand_id': 1, 'brand_name': 'MR D.I.Y'},
+        {'brand_id': 2, 'brand_name': 'KKV'}
+    ])
+
+    # 2. Segments
+    dim_customer_segment = pd.DataFrame([
+        {'segment_id': 1, 'segment_name': 'High-Value'},
+        {'segment_id': 2, 'segment_name': 'Bulk Buyers'},
+        {'segment_id': 3, 'segment_name': 'Regulars'},
+        {'segment_id': 4, 'segment_name': 'Promo-Driven'},
+        {'segment_id': 5, 'segment_name': 'New Customers'}
+    ])
+
+    # 3. Channels
+    dim_channel = pd.DataFrame([
+        {'channel_id': 1, 'channel_name': 'In-Store', 'channel_type_id': 1},
+        {'channel_id': 2, 'channel_name': 'WhatsApp', 'channel_type_id': 2},
+        {'channel_id': 3, 'channel_name': 'META', 'channel_type_id': 2},
+        {'channel_id': 4, 'channel_name': 'Website', 'channel_type_id': 2}
+    ])
+
+    # 4. Promo Dependency
+    dim_promo_dependency = pd.DataFrame([
+        {'promo_dependency_id': 1, 'promo_type': 'Non-Promo', 'description': 'Full Price Items'},
+        {'promo_dependency_id': 2, 'promo_type': 'Seasonal Sale', 'description': 'Festive/Seasonal Discounts'},
+        {'promo_dependency_id': 3, 'promo_type': 'Member Exclusive', 'description': 'Loyalty Program Offers'},
+        {'promo_dependency_id': 4, 'promo_type': 'Flash Sale', 'description': 'Limited Time Deals'}
+    ])
+
+    return {
+        "dim_brand": dim_brand,
+        "dim_customer_segmentation": dim_customer_segment,
+        "dim_channel": dim_channel,
+        "dim_promo_dependency": dim_promo_dependency
+    }
